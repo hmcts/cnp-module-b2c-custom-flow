dccaef36e729e799d3ca442321c91b36ed3ef438
Matt Slater <72858053+msl8r@users.noreply.github.com>
2022-11-17 20:24:47 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,38 @@
 data "azurerm_client_config" "current" {}
 locals {
-  tenant_id = var.tenant_id != "" ? var.tenant_id : data.azurerm_client_config.current.tenant_id
-  client_id = var.client_id != "" ? var.client_id : data.azurerm_client_config.current.object_id
+  tenant_id       = var.tenant_id != "" ? var.tenant_id : data.azurerm_client_config.current.tenant_id
+  client_id       = var.client_id != "" ? var.client_id : data.azurerm_client_config.current.object_id
   b2c_policy_path = "${path.module}/B2CAssets"
-  script_name = "b2c_custom_policy.ps1"
+  script_name     = "b2c_custom_policy.ps1"
 }
 
 resource "local_file" "policy_trust_framework_base" {
     content     = var.policy_trust_framework_base_content
-    filename = "${local.b2c_policy_path}/TrustFrameworkBase.xml"
+    filename    = "${local.b2c_policy_path}/TrustFrameworkBase.xml"
 }
 resource "local_file" "policy_trust_framework_localization" {
     content     = var.policy_trust_framework_localization_content
-    filename = "${local.b2c_policy_path}/TrustFrameworkLocalization.xml"
+    filename    = "${local.b2c_policy_path}/TrustFrameworkLocalization.xml"
 }
 resource "local_file" "policy_trust_framework_extensions" {
     content     = var.policy_trust_framework_extensions_content
-    filename = "${local.b2c_policy_path}/TrustFrameworkExtensions.xml"
+    filename    = "${local.b2c_policy_path}/TrustFrameworkExtensions.xml"
 }
 resource "local_file" "policy_trust_framework_extensions_passwordless" {
     content     = var.policy_trust_framework_extensions_passwordless_content
-    filename = "${local.b2c_policy_path}/TrustFrameworkExtensionsPasswordless.xml"
+    filename    = "${local.b2c_policy_path}/TrustFrameworkExtensionsPasswordless.xml"
 }
 resource "local_file" "policy_sign_up_or_sign_in" {
     content     = var.policy_sign_up_or_sign_in_content
-    filename = "${local.b2c_policy_path}/SignUpOrSignin.xml"
+    filename    = "${local.b2c_policy_path}/SignUpOrSignin.xml"
 }
 resource "local_file" "policy_profile_edit" {
     content     = var.policy_profile_edit_content
-    filename = "${local.b2c_policy_path}/ProfileEdit.xml"
+    filename    = "${local.b2c_policy_path}/ProfileEdit.xml"
 }
 resource "local_file" "policy_password_reset" {
     content     = var.policy_password_reset_content
-    filename = "${local.b2c_policy_path}/PasswordReset.xml"
+    filename    = "${local.b2c_policy_path}/PasswordReset.xml"
 }
 
 resource "null_resource" "powershell" {
