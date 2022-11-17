6b25fff89de0b58bfd9681726c92b00777b34e85
Matt Slater <72858053+msl8r@users.noreply.github.com>
2022-11-17 20:23:20 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,53 +1,54 @@
 variable "tenant_id"{
-  type = string
+  type        = string
   description = "Tenant ID for Azure B2C. If empty it will use the current context."
-  default = ""
+  default     = ""
 }
 
 variable "client_id"{
-  type = string
+  type        = string
   description = "Client ID with permissions on Target Azure B2C. If empty then will use current attached ID."
-  default = ""
+  default     = ""
 }
+
 variable "client_secret"{
-  type = string
+  type        = string
   description = "Client Secret for client ID."
-  default = ""
-  sensitive = true
+  default     = ""
+  sensitive   = true
 }
 
 variable "policy_trust_framework_base_content" {
-  type = string
+  type        = string
   description = "Trust Framework Base XML content"
-  default = ""
+  default     = ""
 }
 variable "policy_trust_framework_localization_content" {
-  type = string
+  type        = string
   description = "Trust Framework Localization XML content"
-  default = ""
+  default     = ""
 }
 variable "policy_trust_framework_extensions_content" {
-  type = string
+  type        = string
   description = "Trust Framework Extensions XML content"
-  default = ""
+  default     = ""
 }
 variable "policy_trust_framework_extensions_passwordless_content" {
-  type = string
+  type        = string
   description = "Trust Framework Extensions Passwordless XML content"
-  default = ""
+  default     = ""
 }
 variable "policy_sign_up_or_sign_in_content" {
-  type = string
+  type        = string
   description = "Sign Up Or Signin XML content"
-  default = ""
+  default     = ""
 }
 variable "policy_profile_edit_content" {
-  type = string
+  type        = string
   description = "Profile Edit XML content"
-  default = ""
+  default     = ""
 }
 variable "policy_password_reset_content" {
-  type = string
+  type        = string
   description = "Password Reset XML content"
-  default = ""
+  default     = ""
 }
